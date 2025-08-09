ca6e94fe4b775ee1fa488c72cd7785d24c384cba
Meralyn  Ga-as <gaasmeralyn25@gmail.com>
2025-08-09 02:23:29 -0600

    Add files via upload

---
+++ basic_calculator.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sol = input("Choose Operator (+, -, *, /): ")
+
+if sol == "+":
+    print(f"{num1} + {num2} = ",num1 + num2)
+elif sol == "-":
+    print(f"{num1} - {num2} = ",num1 - num2)
+elif sol == "*":
+    print(f"{num1} * {num2} = ",num1 * num2)
+elif sol == "/":
+    if num2 != 0:
+        print(f"{num1} / {num2} = ",num1 / num2)
+    else:
+        print("Error: Division by zero")
+else:
+    print("Invalid Operator")
+    
