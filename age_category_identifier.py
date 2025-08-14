999ea79355430fdddc58b908e038659d7aedbc34
Meralyn  Ga-as <gaasmeralyn25@gmail.com>
2025-08-14 09:20:12 -0600

    Add files via upload

---
+++ age_category_identifier.py
@@ -0,0 +1,11 @@
+
+age = int(input("Enter your age: "))
+
+if age <= 12:
+    print("You are a Child.")
+elif age <= 19:
+    print("You are a Teenager.")
+elif age <= 59:
+    print("You are an Adult.")
+else:
+    print("You are a Senior Citizen.")
